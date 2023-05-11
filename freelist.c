bca8acf8145e83122c5c69b9cc8093287408b4bd
Gaburon <124399167+Gaburon@users.noreply.github.com>
2023-05-11 03:09:47 -0500

    freelist.c

--- freelist.c
+++ freelist.c
@@ -19,3 +19,19 @@ void free_stack(stack_t *head)
 		free(node);
 	}
 }
+
+/**
+ * free_list -     frees getline list
+ * @a:             pointer to array of strings
+ * Return:         Nothing
+ */
+void free_list(char *a[])
+{
+	int i = 0;
+
+	while (a[i] != NULL)
+	{
+		free(a[i]);
+		i++;
+	}
+}
