b9f8937397694e95ae77d667b194dd0cb6bfe622
Gaburon <124399167+Gaburon@users.noreply.github.com>
2023-05-11 03:15:00 -0500

    Update montypt1.c

--- montypt1.c
+++ montypt1.c
@@ -46,3 +46,58 @@ void push(stack_t **stack, unsigned int line_num, int n)
 		h->prev = new;
 	*stack = new;
 }
+
+
+/**
+ * pop -           Removes the top element of the stack
+ * @stack:         pointer to head of stack
+ * @line_num:      file's line number
+ * Return:         Void
+ */
+
+void pop(stack_t **stack, unsigned int line_num)
+{
+	stack_t *h = *stack;
+
+	if (!(*stack))
+	{
+		fprintf(stderr, "L%u: can't pop an empty stack\n", line_num);
+		exit(EXIT_FAILURE);
+	}
+
+
+	if (h)
+	{
+		*stack = (h)->next;
+		free(h);
+	}
+}
+
+/**
+ * swap -          Swaps the top two elements of the stack
+ * @stack:         pointer to head of stack
+ * @line_num:      file's line number
+ * Return:         Void
+ */
+void swap(stack_t **stack, unsigned int line_num)
+{
+	stack_t *h = *stack, *ptr;
+
+	if ((*stack) == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%u: can't swap, stack too short\n", line_num);
+		exit(EXIT_FAILURE);
+	}
+
+	if (h && h->next)
+	{
+		ptr = h->next;
+		if (ptr->next)
+			ptr->next->prev = h;
+		h->next = ptr->next;
+		ptr->prev = NULL;
+		ptr->next = h;
+		h->prev = ptr;
+		*stack = ptr;
+	}
+}
