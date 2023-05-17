da1d5158fa37cdf0aca9260e73e46d1d528d5708
Gaburon <124399167+Gaburon@users.noreply.github.com>
2023-05-17 09:14:43 -0500

    main.c

--- main.c
+++ main.c
@@ -1,36 +1,85 @@
-#include "monty.h"
+#include "main.h"
+/**
+ * main - monty interperter
+ * @ac: the number of arguments
+ * @av: the arguments
+ * Return: void
+ */
+int main(int ac, char *av[])
+{
+        stack_t *stack = NULL;
+        static char *string[3000] = {NULL};
+        int n = 0;
+        FILE *fd;
+        size_t bufsize = 3000;
+
+        if (ac != 2)
+        {
+                fprintf(stderr, "USAGE: monty file\n");
+                exit(EXIT_FAILURE);
+        }
+        fd = fopen(av[1], "r");
+        if (fd == NULL)
+        {
+                fprintf(stderr, "Error: Can't open file %s\n", av[1]);
+                exit(EXIT_FAILURE);
+        }
+
+
+        for (n = 0; getline(&(string[n]), &bufsize, fd) > 0; n++)
+                ;
+        execute(string, stack);
+        free_list(string);
+        fclose(fd);
+        return (0);
+}
 
 /**
- * getopcode_fun - gets the function for a given opcode
- * @opcode: operation code to search
- * Return: Pointer to opcode function
+ * execute - executes opcodes
+ * @string: contents of file
+ * @stack: the list
+ * Return: void
  */
 
-void (*getopcode_fun(char *opcode))(stack_t **, unsigned int)
+void execute(char *string[], stack_t *stack)
 {
-	int i = 0;
-	instruction_t made_opcodes[] = {
-		{"push", op_push},
-		{"pall", op_pall},
-		{"pint", op_pint},
-		{"pop", op_pop},
-		{"swap", op_swap},
-		{"add", op_add},
-		{"nop", op_nop},
-		{"sub", op_sub},
-		{"div", op_div},
-		{"mul", op_mul},
-		{"mod", op_mod},
-		{"#", op_nop},
-		{"pchar", op_pchar},
-		{"pstr", op_pstr}
-	};
+        int ln, n, i;
 
-	while (i <= 13)
-	{
-		if (_strcmp(made_opcodes[i].opcode, opcode) == 0)
-			return (made_opcodes[i].f);
-		i++;
-	}
-	return (NULL);
+        instruction_t st[] = {
+                {"pall", pall},
+                {"pint", pint},
+                {"add", add},
+                {"swap", swap},
+                {"pop", pop},
+                {"null", NULL}
+        };
+
+        for (ln = 1, n = 0; string[n + 1]; n++, ln++)
+        {
+                if (_strcmp("push", string[n]))
+                        push(&stack, ln, pushint(string[n], ln));
+                else if (_strcmp("nop", string[n]))
+                        ;
+                else
+                {
+                        i = 0;
+                        while (!_strcmp(st[i].opcode, "null"))
+                        {
+                                if (_strcmp(st[i].opcode, string[n]))
+                                {
+                                        st[i].f(&stack, ln);
+                                        break;
+                                }
+                                i++;
+                        }
+                        if (_strcmp(st[i].opcode, "null") && !_strcmp(string[n], "\n"))
+                        {
+                                fprintf(stderr, "L%u: unknown instruction %s", ln, string[n]);
+                                if (!nlfind(string[n]))
+                                        fprintf(stderr, "\n");
+                                exit(EXIT_FAILURE);
+                        }
+                }
+        }
+        free_stack(stack);
 }
