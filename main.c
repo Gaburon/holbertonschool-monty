f6c11be9aa45efce09da8e18e974e7f3896c1a3c
Gaburon <124399167+Gaburon@users.noreply.github.com>
2023-05-19 12:25:17 -0500

    Update main.c

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include "monty.h"
+
 /**
  * main - monty interperter
  * @ac: the number of arguments
@@ -7,32 +8,31 @@
  */
 int main(int ac, char *av[])
 {
-        stack_t *stack = NULL;
-        static char *string[3000] = {NULL};
-        int n = 0;
-        FILE *fd;
-        size_t bufsize = 3000;
+	stack_t *stack = NULL;
+	static char *string[4069] = {NULL};
+	int n = 0;
+	FILE *fd;
+	size_t bufsize = 4069;
 
-        if (ac != 2)
-        {
-                fprintf(stderr, "USAGE: monty file\n");
-                exit(EXIT_FAILURE);
-        }
-        fd = fopen(av[1], "r");
-        if (fd == NULL)
-        {
-                fprintf(stderr, "Error: Can't open file %s\n", av[1]);
-                exit(EXIT_FAILURE);
-        }
+	if (ac != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+	fd = fopen(av[1], "r");
+	if (fd == NULL)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", av[1]);
+		exit(EXIT_FAILURE);
+	}
 
 
-        for (n = 0; getline(&(string[n]), &bufsize, fd) > 0; n++)
-                ;
-        n--;
-        execute(string, stack);
-        free_list(string);
-        fclose(fd);
-        return (0);
+	for (n = 0; getline(&(string[n]), &bufsize, fd) > 0; n++)
+		;
+	execute(string, stack);
+	free_list(string);
+	fclose(fd);
+	return (0);
 }
 
 /**
