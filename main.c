073c58aae4a697be48b0fa703eba9fe18fe49313
Gaburon <124399167+Gaburon@users.noreply.github.com>
2023-05-19 12:27:55 -0500

    Update main.c

--- main.c
+++ main.c
@@ -36,51 +36,51 @@ int main(int ac, char *av[])
 }
 
 /**
- * execute - executes opcodes
- * @string: contents of file
- * @stack: the list
- * Return: void
+ * execute -       executes opcodes
+ * @string:        contents of file
+ * @stack:         the list
+ * Return:         void
  */
 
 void execute(char *string[], stack_t *stack)
 {
-        int ln, n, i;
+	int ln, n, i;
 
-        instruction_t st[] = {
-                {"pall", pall},
-                {"pint", pint},
-                {"add", add},
-                {"swap", swap},
-                {"pop", pop},
-                {"null", NULL}
-        };
+	instruction_t st[] = {
+		{"pall", pall},
+		{"pint", pint},
+		{"add", add},
+		{"swap", swap},
+		{"pop", pop},
+		{"null", NULL}
+	};
 
-        for (ln = 1, n = 0; string[n + 1]; n++, ln++)
-        {
-                if (_strcmp("push", string[n]))
-                        push(&stack, ln, pushint(string[n], ln));
-                else if (_strcmp("nop", string[n]))
-                        ;
-                else
-                {
-                        i = 0;
-                        while (!_strcmp(st[i].opcode, "null"))
-                        {
-                                if (_strcmp(st[i].opcode, string[n]))
-                                {
-                                        st[i].f(&stack, ln);
-                                        break;
-                                }
-                                i++;
-                        }
-                        if (_strcmp(st[i].opcode, "null") && !_strcmp(string[n], "\n"))
-                        {
-                                fprintf(stderr, "L%u: unknown instruction %s", ln, strtok(string[n]," "));
-                                if (!nlfind(string[n]))
-                                        fprintf(stderr, "\n");
-                                exit(EXIT_FAILURE);
-                        }
-                }
-        }
-        free_stack(stack);
+	for (ln = 1, n = 0; string[n + 1]; n++, ln++)
+	{
+		if (_strcmp("push", string[n]))
+			push(&stack, ln, pushint(string[n], ln));
+		else if (_strcmp("nop", string[n]))
+			;
+		else
+		{
+			i = 0;
+			while (!_strcmp(st[i].opcode, "null"))
+			{
+				if (_strcmp(st[i].opcode, string[n]))
+				{
+					st[i].f(&stack, ln);
+					break;
+				}
+				i++;
+			}
+			if (_strcmp(st[i].opcode, "null") && !_strcmp(string[n], "\n"))
+			{
+				fprintf(stderr, "L%u: unknown instruction %s", ln, strtok(string[n]," "));
+				if (!nlfind(string[n]))
+					fprintf(stderr, "\n");
+				exit(EXIT_FAILURE);
+			}
+		}
+	}
+	free_stack(stack);
 }
